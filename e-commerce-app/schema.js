6f51984a0ed937c8cc06a2e760beee5ca27f6657
José Luis Alavez Manzano <jose.alavez@softtek.com>
2023-04-30 00:44:51 -0600

    Se agregan nuevas relaciones en el schema

---
+++ e-commerce-app/schema.js
@@ -0,0 +1,32 @@
+
+exports.typeDefs = gql`
+    type Query {
+        hello: [String!]!
+        numberOfAnimals: Int
+        price: Float
+        isCool: Boolean
+        products: [Product!]!
+        product(id: ID!): Product
+        categories: [Category!]!
+        category(id: ID!): Category
+    }
+
+    type Product {
+        id: ID!
+        name: String!
+        description: String!
+        quantity: Int!
+        price: Float!
+        image: String!
+        onSale: Boolean! 
+        category: Category
+    }
+
+    type Category {
+      id: ID!
+      name: String!
+      products: [Product!]!
+    }
+`
+
+
