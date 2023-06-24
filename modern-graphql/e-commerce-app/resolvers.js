2e7e95a9db725278cf2592343544240aeb63ecb6
Jose Luis Alavez <itprotopics@gmail.com>
2023-06-24 14:03:36 -0600

    fdsafasd

--- modern-graphql/e-commerce-app/resolvers.js
+++ modern-graphql/e-commerce-app/resolvers.js
@@ -1,18 +1,24 @@
 
 export const resolvers = {
+    // Query: {
+    //     hello: () => {
+    //         return "World!";
+    //     },
+    //     numberOfAnimals:  () => {
+    //         return 55;
+    //     },
+    //     price: () => {
+    //         return 2334343.4555;
+    //     },
+    //     isCool: () => {
+    //         return false;
+    //     } 
+    // }   
+
     Query: {
         hello: () => {
-            return "World!";
-        },
-        numberOfAnimals:  () => {
-            return 55;
-        },
-        price: () => {
-            return 2334343.4555;
-        },
-        isCool: () => {
-            return false;
-        } 
-    }   
+            return ["Hello", "my", "friend"];
+        }
+    }
 }
 
--- modern-graphql/e-commerce-app/typeDefs.js
+++ modern-graphql/e-commerce-app/typeDefs.js
@@ -4,13 +4,14 @@ import { gql } from "apollo-server";
 // Scalar types: String, Int, Float, Boolean
 
 export const typeDefs = gql`
+    # type Query {
+    #     hello: String!
+    #     numberOfAnimals: Int
+    #     price: Float
+    #     isCool: Boolean
+    # }
+
     type Query {
-        hello: String!
-        numberOfAnimals: Int
-        price: Float
-        isCool: Boolean
+        hello: [String!]
     }
-
-
-
 `;
